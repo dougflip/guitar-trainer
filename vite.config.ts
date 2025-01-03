5487a0dc11c1fb2d527e40534ae3074f8ae44858
dougflip <douglasdifilippo@gmail.com>
2025-01-03 18:18:33 -0500

    Adds deployment to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  base: "/whats-the-score-instant/",
 });
