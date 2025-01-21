79e67ee7d80044bc032b557e8e9bc12ca81e4cbe
dougflip <douglasdifilippo@gmail.com>
2025-01-20 20:04:14 -0500

    Remove tanstack dev tools
    Can always add back conditionally if needed

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,10 @@
 import { MainAppShell } from "@/components/layout/MainAppShell";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { createRootRoute } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <MainAppShell />
-      <TanStackRouterDevtools />
     </>
   ),
 });
